6620d4a81af8c1a15020d517af3cfce6f05cf5e7
MdAsraful56 <work.mdasraful56@gmail.com>
2025-02-19 22:34:27 +0600

    function wishe complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import "./App.css";
 import useCurrencyInfo from "./hooks/useCurrencyInfo";
 import { InputBox } from "./components";
+import { useState } from "react";
 
 function App() {
+
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("USD");
+  const [to, setTo] = useState("INR");
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(from);
+
+  const options = Object.keys(currencyInfo);
+
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+    setAmount(convertedAmount);
+    setConvertedAmount(amount);
+  }
+
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to]);
+  }
+
+
   return (
     <>
-      <h1 className="text-4xl text-center">Hello World</h1>
+      
     </>
   );
 }
